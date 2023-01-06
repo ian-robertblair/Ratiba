3f5981490f870b2baec269af61fd482ced0dad2e
ian-robertblair <ianrobertblair@icloud.com>
2023-01-06 10:01:37 +0800

    Create project and added groups

---
+++ Ratiba/Ratiba/RatibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatibaApp.swift
+//  Ratiba
+//
+//  Created by ian robert blair on 2023/1/6.
+//
+
+import SwiftUI
+
+@main
+struct RatibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
